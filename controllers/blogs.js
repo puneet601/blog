24202fac84d72d917e32797796df4af54f66519a
Puneet K Jattana <puneetkaurjattana@gmail.com>
2021-01-16 17:38:43 +0530

    deploying

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,6 +2,7 @@ const blogRouter= require('express').Router()
 const Blog=require('../models/blog')
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
+const { request } = require('express')
 const getTokenFrom = request => {
   const authorization = request.get('authorization')
   if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
@@ -17,19 +18,19 @@ blogRouter.get('/', async(request, response) => {
 blogRouter.get('/:id', async (request, response) => {
   const blog = await Blog.findById(request.params.id).populate('user', { username: 1, name: 1 })
   response.json(blog)
-  })
+})
   
-  blogRouter.post('/', async(request, response) => {
-    const body = request.body
-    const token = getTokenFrom(request)
+  
+blogRouter.post('/', async (request, response) => {
+  const body = request.body
+  const token = getTokenFrom(request)
     const decodedToken = jwt.verify(token, process.env.SECRET)
     if (!token || !decodedToken.id) {
       return response.status(401).json({ error: 'token missing or invalid' })
     }
     const user = await User.findById(decodedToken.id)
-    // const user = await User.findById(body.userId)
     if (body.title === undefined || body.url === undefined || body.author === undefined)
-      response.status(400).end()
+    response.status(400).end()
    const newBlog = new Blog({
       title: body.title,
       author: body.author,
@@ -37,10 +38,11 @@ blogRouter.get('/:id', async (request, response) => {
      likes: body.likes || 0,
      user: user._id
    })
-   user.blogs = user.blogs.concat(newBlog._id)
+   
+  const savedBlog = await newBlog.save()
+  user.blogs = user.blogs.concat(newBlog._id)
    await user.save()
-    const savedBlog = await newBlog.save()
-    console.log(newBlog.user)
+  console.log(newBlog.user)
       response.json(savedBlog)
   })
   
@@ -67,15 +69,36 @@ blogRouter.delete('/:id', async (request, response,next) => {
 })
  
 blogRouter.put('/:id', async (request, response) => {
-  const body = request.body
-  const newblog = {
-    title: body.title,
-    author: body.author,
-    url: body.url,
-   likes: body.likes +1,
-   user: body.userId
-  }
-  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, newblog,{new:true})
-  response.json(updatedBlog)
-})
+  const { author, title, url, likes } = request.body;
+  const blog = {
+    author,
+    title,
+    url,
+    likes
+  };
+
+  console.log(blog);
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+    new: true
+  });
+
+  console.log("updatedBlog: ", updatedBlog);
+
+  response.json(updatedBlog.toJSON());
+});
+blogRouter.delete('/:id', async (request, response) => {
+  const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+  const user = await User.findById(decodedToken.id)
+  const blog = await Blog.findById(request.params.id)
+  if(blog.user === user.id)
+    {await Blog.findByIdAndDelete(request.params.id)
+    response.status(204)}
+  else
+    response.json("You are not authorized to delete this Blog.")
+  
+  })
   module.exports=blogRouter
--- controllers/users.js
+++ controllers/users.js
@@ -2,10 +2,16 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 const Blog=require('../models/blog')
+const express = require('express')
 usersRouter.get('/', async(request,response) => {
   const users = await User.find({}).populate('blogs')
   response.json(users)
 })
+usersRouter.get('/:id', async (request, response) => {
+  const user = await User.findById(request.params.id)
+  response.json(user.blogs)
+})
+
 usersRouter.post('/', async (request, response,next) => {
   try {
     const body = request.body
